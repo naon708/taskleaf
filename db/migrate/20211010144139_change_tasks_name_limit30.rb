7d6f7ac3137f753e6806f2d3209e855b43bbb13a
naon708 <naoya_nis@icloud.com>
2021-10-10 23:52:21 +0900

    Complete 4-3-5

---
+++ db/migrate/20211010144139_change_tasks_name_limit30.rb
@@ -0,0 +1,13 @@
+class ChangeTasksNameLimit30 < ActiveRecord::Migration[5.2]
+  def change
+  end
+
+  def up
+    change_column :tasks, :name, :string, limit: 30
+  end
+
+  def down
+    change_column :tasks, :name, :string
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_022812) do
+ActiveRecord::Schema.define(version: 2021_10_10_144139) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
